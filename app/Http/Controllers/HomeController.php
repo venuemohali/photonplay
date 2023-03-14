e20d136823f171a4b77ed9d035a517aa6d1dc4a8
muskan <venuemohali@gmail.com>
2023-03-14 16:54:29 +0530

    admin panel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -30,4 +31,6 @@ class HomeController extends Controller
     {
         return redirect('/login');
     }
+
+
 }
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,11 +3,11 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-6 footer-copyright text-start">
-          <p class="mb-0">Copyright © 2022 Company Name. All rights reserved.</p>
+          <p class="mb-0">Copyright © 2023 Photon Play. All rights reserved.</p>
         </div>
         <div class="col-md-6 footer-copyright text-end">
          <a href="#" target="_blank">
-          <p class="mb-0">Designed By _____</p>
+          <p class="mb-0"></p>
         </a>
         </div>
       </div>
