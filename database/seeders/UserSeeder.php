cc5c7129f81153aac87bc2f216e5f00c00f8e055
Vicky Chhetri <specialpower321@gmail.com>
2023-03-28 00:11:10 +0530

    Product Seeder Added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -15,6 +16,8 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::truncate();
+
         DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@abc.com',
