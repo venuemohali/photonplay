98902d8ab9192cbebcda27c06d316a0944538616
Vicky Chhetri <specialpower321@gmail.com>
2023-04-01 10:53:49 +0530

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductSeeder::class,
+            StaticContentSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
     }
