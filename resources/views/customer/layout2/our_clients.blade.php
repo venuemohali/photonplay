789e9983d6c568f69809ef6bea7135a932fd4a9a
satinder <satinderpanesar03@gmail.com>
2023-05-02 21:09:04 +0530

    Signed-off-by: satinder <satinderpanesar03@gmail.com>

---
+++ resources/views/customer/layout2/our_clients.blade.php
@@ -0,0 +1,24 @@
+@php
+use App\Models\Client;
+$clients = Client::get();
+@endphp
+<section class="our-clints-last">
+        <div class="mb-lg-5 text-center">
+            <h2 class="fs-md-2 mt-3">Our Clients</h2>
+        </div>
+        <div class="container">
+            <div class="px-4">
+                <div class="clints-content mb-0 d-flex align-items-center">
+                    @forelse ($clients as $client)
+                    <div>
+                        <div class="px-2 branding-diss">
+                            <img src="{{asset('assets\customer\images\ . "$client->image" . ')}}" class="d-block mx-auto" />
+                        </div>
+                    </div>    
+                    @empty
+                        
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </section>
