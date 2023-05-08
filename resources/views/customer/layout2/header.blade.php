60237e5afab7941dbe5b2cc6230b9808f4e10316
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-05-08 08:13:48 +0530

    mobile nav home icon company link

--- resources/views/customer/layout2/header.blade.php
+++ resources/views/customer/layout2/header.blade.php
@@ -221,13 +221,10 @@
                     <!-- About -->
                     <div class="mega-menu mt-4">
                         <div class="mega-menu-items">
-                            <!-- <ul class="ps-0"><li class="text-uppercase">Home</li></ul> -->
-                            <a href="{{route('customer.homePage')}}"> <h4 class="text-uppercase">Home</h4> </a>
+                            <a href="{{route('customer.homePage')}}" class="text-decoration-none"> <h4 class="text-uppercase">Home</h4> </a>
                         </div>
                         <div class="mega-menu-items">
-                            <!-- <ul class="ps-0"><li class="text-uppercase">Home</li></ul> -->
-
-                            <a href="{{route('customer.about.us')}}"> <h4 class="text-uppercase">Company</h4></a>
+                            <a href="{{route('customer.about.us')}}"  class="text-decoration-none"> <h4 class="text-uppercase">Company</h4></a>
                         </div>
                         <div class="mega-menu-parent">
                             <!-- <ul class="ps-0"><li class="text-uppercase">Company</li></ul> -->
