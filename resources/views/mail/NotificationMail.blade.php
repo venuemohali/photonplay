bd7e4c53fd3f06afb1e6734b900bdea97edab912
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-05-11 15:09:04 +0530

    bnotifcation mail

--- resources/views/mail/NotificationMail.blade.php
+++ resources/views/mail/NotificationMail.blade.php
@@ -1,13 +1,14 @@
 @component('mail::message')
-    <table style="margin: 0 auto;">
+    <table style="width: 100%;">
         <tr>
             <td>
-                <img src='{{ asset('assets/customer/images/logo-dark.png') }}' style="width: 200px; height: 100px;"  alt="Logo"/>
+                <img src='{{ asset('assets/customer/images/logo-dark.png') }}' style="width: 200px; height: 100px;"  style="width:100%;" alt="Logo"/>
             </td>
         </tr>
     </table>
+    <hr/>
     {!! $message->body !!}
-
+    <h4>
     Thanks,<br>
-    {{ config('app.name') }}
+    {{ config('app.name') }}</h4>
 @endcomponent
