e6fb5755c32ef3412bde2769313fd8e4671d65f5
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-05-19 00:02:25 +0530

    working on seo

--- resources/views/components/customer/meta-seo-tag.blade.php
+++ resources/views/components/customer/meta-seo-tag.blade.php
@@ -1,20 +1,43 @@
 @if(isset($seodata['title']))
     <title> {{$seodata['title']}} </title>
     <meta name="title" content="{{$seodata['title']}}">
+    <meta property="og:title" content="{{$seodata['title']}}">
+    <meta property="twitter:title" content="{{$seodata['title']}}">
 @endif
 @if(isset($seodata['description']))
     <meta name="description" content="{{$seodata['description']}}">
+    <meta property="og:description" content="{{$seodata['description']}}">
+    <meta property="twitter:description" content="{{$seodata['description']}}">
 @endif
 
 @if(isset($seodata['keywords']))
     <meta name="keywords" content="{{$seodata['keywords']}}">
 @endif
 
-<meta name="robots" content="index, follow">
+<link rel = "icon" href =
+    "{{asset('assets/images/photon_small.png')}}"
+      type = "image/x-icon">
+
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://www.photonplay.com/">
+
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://www.photonplay.com/">
+
+
+<!-- Static -->
+<meta name="author" content="Photonplay">
+<meta name="publisher" content="Photonplay">
+<meta property="locale" content="en_US" />
+<meta property="type" content="website" />
+<meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="language" content="English">
 <meta name="revisit-after" content="7 days">
 <meta name="author" content="Photonplay">
-<link rel = "icon" href =
-    "{{asset('assets/images/photon_small.png')}}"
-      type = "image/x-icon">
