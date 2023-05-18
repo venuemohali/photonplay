b6c16e51372d52a3d61afe1526f026a9b0b2e067
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-05-18 11:31:39 +0530

    Login

--- app/Http/Controllers/customer/Auth/LoginController.php
+++ app/Http/Controllers/customer/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Customer;
 use Illuminate\Auth\Events\Validated;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -124,8 +125,7 @@ class LoginController extends Controller
                 'provider'      => $provider,
             ]);
 
-            }
-            catch (\Illuminate\Database\QueryException $e) {
+            }catch (QueryException $e) {
                 if ($e->errorInfo[1] == 1062) {
                     return back()->with('error', 'This email has been registered by normal signup.');
                 } else {
