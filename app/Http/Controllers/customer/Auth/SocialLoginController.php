228835f5b66178d8f4e76d9ae4c3383ad52f97b0
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-05-18 11:40:44 +0530

    Login error

--- app/Http/Controllers/customer/Auth/SocialLoginController.php
+++ app/Http/Controllers/customer/Auth/SocialLoginController.php
@@ -52,15 +52,10 @@ class SocialLoginController extends Controller
                 return redirect()->intended('radar-speed-signs');
             }
 
-        }catch(\Illuminate\Database\QueryException $e) {
-            if ($e->errorInfo[1] == 1062) {
-                return back()->with('error', 'This email has been registered by normal signup.');
-            } else {
-                return back()->with('error', $e->getMessage());
-            }
         }
         catch (\Exception $e) {
-            dd($e->getMessage());
+//            dd($e->getMessage());
+            return back()->with('error', 'This email has been registered by normal signup.');
         }
     }
 }
