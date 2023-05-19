f914f7ded088cb4f35db6af3293b4df898247e23
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-05-19 10:37:08 +0530

    lancontrol freatures

--- resources/views/customer/lane_control_system.blade.php
+++ resources/views/customer/lane_control_system.blade.php
@@ -92,15 +92,31 @@ $seo_meta=[
             <h2 class="fs-md-2 mt-3 text-center">Feature</h2>
             <div class="feature-list">
                 <!-- <ul class=" m-0 p-0 "> -->
-                <<div class="d-lg-flex justify-content-between">
-                    <ul class="w-100">
+                <div class="d-lg-flex justify-content-between">
+                    <ul class="w-100 m-0 p-0">
+                        @php
+                            $break_point=(int)(count($page->features)/2);
+                            $sr=1;
+
+                        @endphp
+
                         @foreach($page->features as $feature)
                             <li>
-                                <div class="content-feature">
-                                    <strong>{{$feature->feature}}</strong>
+                                <div class="content-feature" style="width: 300px;">
+                                    <strong>{{ $feature->feature}}</strong>
                                     <span>{{$feature->description}}</span>
                                 </div>
                             </li>
+                            @if($sr==$break_point)
+                    </ul>
+                    <ul class="w-100 m-0 p-0">
+                        @php
+                            $sr=0;
+                        @endphp
+                        @endif
+                        @php
+                            $sr++;
+                        @endphp
                         @endforeach
                     </ul>
                 </div>
