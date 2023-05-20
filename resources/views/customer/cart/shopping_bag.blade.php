0dc37370b00779efb09c82fabf7f56f8260e76aa
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-05-20 11:52:57 +0530

    image size radar height

--- resources/views/customer/cart/shopping_bag.blade.php
+++ resources/views/customer/cart/shopping_bag.blade.php
@@ -140,7 +140,7 @@
     </div>
 </section>
 
-@include('customer.layouts.footer')
+@include('customer.layout2.footer')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
   $(document).ready(function(){
