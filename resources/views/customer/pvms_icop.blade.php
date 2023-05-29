db80c99bab4a0d368da1de532cbfb7d28f04b1d6
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-05-29 15:27:44 +0530

    $brochure->brochure

--- resources/views/customer/pvms_icop.blade.php
+++ resources/views/customer/pvms_icop.blade.php
@@ -30,10 +30,10 @@ $seo_meta=[
                                 <div class="fs-6 mt-md-4">
                                     <p class="text-dark">No. 1 in Traffic Calming Solution</p>
                                 </div>
-                                <button type="button"
+                                <a href="{{asset("storage/".$brochure->brochure)}} " type="button"
                                     class="py-2 rounded border-0 px-4 mt-5 bg-white outline-0 btn-light">Download
                                     Brochure <img class="fs-4 ms-2" width="10" src="{{asset('assets\customer\images\downarrow.png')}}"
-                                        alt=""></button>
+                                        alt=""></a>
                                 <div class="zigzack d-flex justify-content-start"><img src="{{asset('assets\customer\images\ziczac.png')}}"
                                         class="img-fluid d-none d-md-block" alt="not-found"></div>
                                 <div
