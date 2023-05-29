37aee9c4dde5750ba8c61292f62d4586c6f0d42d
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-05-30 01:06:46 +0530

    load filter wise category data

--- resources/views/customer/about_us.blade.php
+++ resources/views/customer/about_us.blade.php
@@ -13,7 +13,7 @@ $seo_meta=[
     <section class="pt-0 pb-sm-4 pb-lg-5">
         <div class="banner">
             <div class="about-wrapper d-flex justify-content-center align-items-center">
-                <h1> About us</h1>
+                <h3 class="text-dark"> About us</h3>
             </div>
         </div>
         <!-- </div> -->
