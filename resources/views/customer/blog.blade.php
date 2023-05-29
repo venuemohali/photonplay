d20bed03175815964357b4a5a78c4dbf164f6a90
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-05-29 20:54:59 +0530

    text deocratin none

--- resources/views/customer/blog.blade.php
+++ resources/views/customer/blog.blade.php
@@ -162,7 +162,7 @@ $seo_meta=[
                     <h6 class="mb-4 text-uppercase">Related Posts</h6>
                 </div>
                 <div class="rules-content mb-0 d-flex align-items-center border-0 border-bottom pb-5">
-
+                    <div>
                    @foreach($relatedBlogRecords as $relatedBlog)
                         <a href="{{route('customer.blog_show',$relatedBlog->slug)}}" class="text-decoration-none">
                     <div>
@@ -179,7 +179,7 @@ $seo_meta=[
                         </a>
 
                     @endforeach
-
+                    </div>
                 </div>
             </div>
         </div>
