1978921f32f76a78389ff6a1313e805bb4668e82
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-06-01 08:02:54 +0530

    menu show center

--- resources/views/customer/pvms_icop.blade.php
+++ resources/views/customer/pvms_icop.blade.php
@@ -12,10 +12,10 @@ $seo_meta=[
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
                     aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
+{{--                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"--}}
+{{--                    aria-label="Slide 2"></button>--}}
+{{--                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"--}}
+{{--                    aria-label="Slide 3"></button>--}}
             </div>
             <div class="carousel-inner">
                 @for ($i=1; $i<=3; $i++)
