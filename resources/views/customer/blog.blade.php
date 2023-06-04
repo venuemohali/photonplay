5fc4e04d80c48b904d4813fa5af2b6166c9a8e83
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-06-04 16:11:14 +0530

    tags

--- resources/views/customer/blog.blade.php
+++ resources/views/customer/blog.blade.php
@@ -129,7 +129,7 @@ $seo_meta=[
                                 <div class="mb-3">
                                     <?php $i=1;?>
                                 @foreach($tags as $tag)
-                                        <span style="font-size: 10px;border: grey 1px solid;">
+                                        <span >
                                             <a href="/blog?tags={{strtolower($tag)}}" class="text-decoration-none">{{$tag}}</a></span>
                                     @if($i%3==0)
                                 </div>
