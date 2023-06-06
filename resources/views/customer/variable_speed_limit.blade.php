4241cefe06dcd0ea1b9f49a3c9c76d0a1a8d18b0
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-06-06 10:45:53 +0530

    vcs

--- resources/views/customer/variable_speed_limit.blade.php
+++ resources/views/customer/variable_speed_limit.blade.php
@@ -68,6 +68,7 @@ $seo_meta=[
 
                                         <div id="modalOverlay" class="modal-overlay" onclick="hideModal()">
                                             <div id="modalContent" class="modal-content">
+                                                <span class="closeImage" onclick="hideModal()">&times;</span> <!-- Add the close symbol here -->
                                                 <img id="myImage" src="{{asset('assets/customer/images/zoom-in.png')}}" alt="Image" class="image-size">
                                             </div>
                                         </div>
