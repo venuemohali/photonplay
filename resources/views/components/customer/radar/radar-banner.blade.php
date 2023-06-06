90cad4e990ce8b256ad170f2c603050b4132fac8
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2023-06-06 20:22:49 +0530

    color and button home

--- resources/views/components/customer/radar/radar-banner.blade.php
+++ resources/views/components/customer/radar/radar-banner.blade.php
@@ -11,8 +11,9 @@
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div class="banner-two">
-                    <div class="banner-image d-flex align-items-center justify-content-around h-100">
+                    <div class="banner-image d-flex align-items-center justify-content-around h-100" style="background-color: rgba(0, 0, 0, 0.4); height: 100%;">
                         <div class="slider-heading">
+                            <center> <img src="https://stagingserver.photonplay.com/assets/customer/images/KEPLER-US-12.png"  style="height: 200px;" class="text-center"/></center>
                             <h1 class="mb-lg-3" style="text-shadow: black 2px 2px;">iCop | RADAR SPEED SIGN</h1>
                             <h6 style="text-shadow: black 1px 1px;">No.1 In Traffic Claming Solution</h6>
                             <p class="mb-0" style="text-shadow: black 1px 1px;">Best Safety Score with standout visibility and features,</p>
--- resources/views/customer/sign.blade.php
+++ resources/views/customer/sign.blade.php
@@ -18,6 +18,7 @@ $seo_meta=[
                     <div class="radar-icop">
 {{--                        <h6 class="fs-6 mb-3">photonplay’s</h6>--}}
 {{--                        <h1 class="mb-">RADAR SPEED SIGN <br> iCop Series </h1>--}}
+
                         <h4 class="text-uppercase text-center">MOST CAPABLE AND HIGHLY-EQUIPPED RADAR SPEED SIGN EVER BUILT !</h4>
                         <p class="mt-4">
                             The sign has all the standard features combined in one product to make it industry's best
